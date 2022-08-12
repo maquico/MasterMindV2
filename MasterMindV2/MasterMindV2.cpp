66125ce8a22046bed0d2662dbb17e0f7587b0a0b
LAPTOP-JC0100HH\Chawillfer <chawillfer@gmail.com>
2022-08-12 16:15:53 -0400

    Cambio William

--- MasterMindV2/MasterMindV2.cpp
+++ MasterMindV2/MasterMindV2.cpp
@@ -59,22 +59,32 @@ string GenerarNumRand() {
 int ValidarClave(string intento) {
 	
 	
-	// 0 indica longitud erronea, 1 indica repetidos, 2 indica que hay letras, 3 indica que la clave es valida
+	// 0 indica longitud erronea, 1 indica repetidos, 2 indica que hay letras, 3 indica que la clave tiene numeros diferentes de 1, 2, 3, 4, 5, 6
 	bool hayLetras = false;
+	bool limitenumeros = false;
 	
 		for (int i = 0; i < 4; i++)
 		{
 			if (!isdigit(intento[i]))
 			{
+				
 				hayLetras = true;
 			}
-			
+			char numero[1] = { intento[i] };
+			if (atoi(numero) < 0 || atoi(numero) > 6)
+			{
+				limitenumeros = true;
+			}
 		}
 	
-	if (hayLetras)
-	{
-		return 2;
-	}
+		if (hayLetras)
+		{
+			return 2;
+		}
+		else if (limitenumeros) 
+		{
+			return 3;
+		}
 	else if (intento.length() == 4) 
 	{
 		for (int i = 0; i < 4; i++)
@@ -126,6 +136,10 @@ int Jugar(string clave) {
 			//hay letras
 			cout << "Por favor no pongas letras\n\n";
 			i--;
+			break;
+		case 3: 
+			//Numero menor que 1 o mayor que 6
+			cout << "La clave no puede tener numeros menores que 1 o mayores que 6\n\n";
 			break;
 		default:
 			cout << "Duro\n\n";
