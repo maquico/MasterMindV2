8d9cac8cfee9e54d22d605af114fb4bac0ee48c6
DESKTOP-B9S7PAR\Angel <angelgmorenor@gmail.com>
2022-08-12 17:46:36 -0400

    Juego Listo con Gotoxy

--- MasterMindV2/MasterMindV2.cpp
+++ MasterMindV2/MasterMindV2.cpp
@@ -15,59 +15,41 @@ AUTORES: ANGEL MORENO ID:1104666
 #include <cstdlib>
 #include <string>
 #include <Windows.h>
+#include <conio.h>
 // #include "TextTable.h"
 
 using namespace std;
 
-int ValidarClave(string intento) {
+int ValidarClave(string intento, string claveActual, int i) {
 	
 	
 	// 0 indica longitud erronea, 1 indica repetidos, 2 indica que hay letras, 3 indica que la clave tiene numeros diferentes de 1, 2, 3, 4, 5, 6
-	bool hayLetras = false;
+	bool hayLetras = false, wlongitud = false;
+	
 	bool limitenumeros = false;
 	
-		for (int i = 0; i < 4; i++)
-		{
-			if (!isdigit(intento[i]))
-			{
-				
-				hayLetras = true;
-			}
-			char numero[1] = { intento[i] };
-			if (atoi(numero) < 0 || atoi(numero) > 6)
-			{
-				limitenumeros = true;
-			}
-		}
-	
-		if (hayLetras)
-		{
-			return 2;
-		}
-		else if (limitenumeros) 
-		{
-			return 3;
-		}
-	else if (intento.length() == 4) 
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = i + 1; j < 4; j++)
-			{
-				if (intento[i] == intento[j])
-				{
-					return 1;
-					
-
-				}
-			}
-		}
-	}
-	else
+	if (intento.length() != 1)
 	{
 		return 0;
 	}
-	
+	else if (!isdigit(intento[0])) {
+		return 2;
+	}
+	else if (stoi(intento) < 0 || stoi(intento) > 6)
+	{
+		return 3;
+	}
+	else if (i!=0 )
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			if (claveActual[j]==intento[0])
+			{
+				return 1;
+			}
+		}
+	}
+			
 }
 
 void GoToXY(int x, int y)
@@ -118,61 +100,11 @@ string GenerarNumRand() {
 }
 
 
-int ValidarClave(string intento) {
-
-
-	// 0 indica longitud erronea, 1 indica repetidos, 2 indica que hay letras, 3 indica que la clave tiene numeros diferentes de 1, 2, 3, 4, 5, 6
-	bool hayLetras = false;
-	bool limitenumeros = false;
-
-	for (int i = 0; i < 4; i++)
-	{
-		if (!isdigit(intento[i]))
-		{
-
-			hayLetras = true;
-		}
-		char numero[1] = { intento[i] };
-		if (atoi(numero) < 0 || atoi(numero) > 6)
-		{
-			limitenumeros = true;
-		}
-	}
-
-	if (hayLetras)
-	{
-		return 2;
-	}
-	else if (limitenumeros)
-	{
-		return 3;
-	}
-	else if (intento.length() == 4)
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = i + 1; j < 4; j++)
-			{
-				if (intento[i] == intento[j])
-				{
-					return 1;
-
-
-				}
-			}
-		}
-	}
-	else
-	{
-		return 0;
-	}
-
-}
 
 int Jugar(string clave) {
 
-	string intento, pista;
-	char cIntento;
+	string claveActual, pista, cIntento;
+	
 	bool numDentro = false;
 	int tabla1x = 18;
 	int tablay = 8;
@@ -208,68 +140,105 @@ int Jugar(string clave) {
 
 	for (int j = 0; j < 10; j++)
 	{
-		if (intento != clave)
+		if (claveActual != clave)
 		{
-			
-			GoToXY(0, 30); 
-			cout << "Numero a ingresar :                                                                ";
-			GoToXY(20, 30);
-			cin >> intento;
-			switch (ValidarClave(intento))
+			claveActual = "";
+			for (int z = 0; z < 4; z++)
 			{
-			case 0:
-				//longitud erronea;
-				cout << "Longitud erronea\n\n";
-				j--;
-				break;
-			case 1:
-				//repetidos
-				cout << "Numeros repetidos\n\n";
-				j--;
-				break;
-			case 2:
-				//hay letras
-				cout << "Por favor no pongas letras\n\n";
-				j--;
-				break;
-			case 3:
-				//Numero menor que 1 o mayor que 6
-				cout << "La clave no puede tener numeros menores que 1 o mayores que 6\n\n";
-				break;
-			default:
-				cout << "Duro\n\n";
-				intento += cIntento;
-				GoToXY(tabla1x, tablay);
-				cout << cIntento;
-				for (int k = 0; k < 4; k++) // clave 6513           intento 6415
+				GoToXY(0, 30);
+				cout << "Numero a ingresar :                                                                ";
+				GoToXY(20, 30);
+				cin >> cIntento;
+				switch (ValidarClave(cIntento, claveActual, z))
 				{
-					for (int l = 0; l < 4 && !numDentro; l++)
+				case 0:
+					//longitud erronea;
+					cout << "Longitud erronea\n";
+					cout << "Presione cualquier tecla para continuar";
+					_getch();
+					GoToXY(0, 31);
+					cout << "                \n";
+					cout << "                                       ";
+					z--;
+					break;
+				case 1:
+					//repetidos
+					cout << "Numeros repetidos\n";
+					cout << "Presione cualquier tecla para continuar";
+					_getch();
+					GoToXY(0, 31);
+					cout << "                 \n";
+					cout << "                                       ";
+					z--;
+					break;
+				case 2:
+					//hay letras
+					cout << "Por favor no pongas letras\n";
+					cout << "Presione cualquier tecla para continuar";
+					_getch();
+					GoToXY(0, 31);
+					cout << "                          \n";
+					cout << "                                       ";
+					z--;
+					break;
+				case 3:
+					//Numero menor que 1 o mayor que 6
+					cout << "La clave no puede tener numeros menores que 1 o mayores que 6\n";
+					cout << "Presione cualquier tecla para continuar";
+					_getch();
+					GoToXY(0, 31);
+					cout << "                                                              \n";
+					cout << "                                                              ";
+					z--;
+					break;
+				default:
+					cout << "Duro\n\n";
+
+					GoToXY(tabla1x, tablay);
+					cout << cIntento;
+					claveActual += cIntento;
+					tabla1x += 6;
+					break;
+				}
+			}
+			for (int k = 0; k < 4; k++) // clave 6513           intento 6415
+			{
+				for (int l = 0; l < 4 && !numDentro; l++)
+				{
+					if (claveActual[k] == clave[l] && k == l)
 					{
-						if (intento[k] == clave[l] && k == l)
-						{
-							pista += "C";
-							numDentro = true;
-						}
-						else if (intento[k] == clave[l])
-						{
-							pista += "F";
-							numDentro = true;
-						}
+						pista += "C";
+						numDentro = true;
 					}
-					if (!numDentro)
+					else if (claveActual[k] == clave[l])
 					{
-						pista += "X";
-					}
-					else
-					{
-						numDentro = false;
+						pista += "F";
+						numDentro = true;
 					}
 				}
+				if (!numDentro)
+				{
+					pista += "X";
+				}
+				else
+				{
+					numDentro = false;
+				}
+				GoToXY(tabla2x, tablay);
 				cout << pista;
 				pista = "";
-				break;
+				tabla2x += 6;
 			}
+			tabla1x = 18;
+			tabla2x = 59;
+			tablay += 2;
+			
 		}
+		else
+		{
+			return 1;
+		}
+
 		}
 		
 	return 0;
