43803d2f012eba2226d605dcb208437b25e2a294
DESKTOP-B9S7PAR\Angel <angelgmorenor@gmail.com>
2022-09-07 17:38:02 -0400

    Creacion proyecto Frecuencia Caracter

---
+++ FrecuenciaCaracter/FrecuenciaCaracter.cpp
@@ -0,0 +1,27 @@
+/*
+* ENUNCIADO: TAREA 6. FRECUENCIAS CARACTER
+* ASUNTO: Determinar la frecuencia de una letra del alfabeto español en
+términos porcentuales. De igual manera el programa debe contrar la cantidad de palabras que
+contiene el archivo de texto.
+AUTORES: ANGEL MORENO ID:1104666
+*        GLEIDY ESPINAL ID:1104225
+*        ALLEN SILVERIO ID:1104220
+*        CRISTIAN JIMÉNEZ ID:1106166
+*        WILLIAM FERREIRA ID:1104228
+* FECHA: 12/09/2022
+*/
+
+
+#include <iostream>
+#include <fstream> //manejo de archivos
+
+
+using namespace std;
+int main()
+{
+    string palabra;
+    char caracter;
+    ifstream archivo("PruebaChar.txt");
+    cout << "Espa" << (char)164 <<"ol";
+}
+
